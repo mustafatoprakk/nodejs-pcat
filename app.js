a53247555061e6d1b6c53eab56971f6b713f4660
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-02-15 09:31:42 +0300

    static files uploaded

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const path = require("path")
+const app=express()
+
+app.use(express.static('public'))
+
+
+app.get("/", (req, res) => {
+    // call index
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+})
+
+
+
+const port=3000;
+app.listen(port, () => {
+    console.log(`Server started ${port} of port`)
+})
