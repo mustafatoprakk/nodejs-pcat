becacb76d6738e44824984367c4c468e53787801
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-02-19 11:23:28 +0300

    anasayfay da her bir resme tıklandığında title ve desc tekil sayfaya gönderildi

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.get('/', async (req, res) => {
   const photos = await Photo.find({});
   res.render('index', { photos });
 });
+app.get('/photos/:id', async (req, res) => {
+  //console.log(req.params.id)
+  const photo=await Photo.findById(req.params.id)
+  res.render("photo",{
+    photo
+  })
+});
 app.get('/about', (req, res) => {
   res.render('about');
 });
